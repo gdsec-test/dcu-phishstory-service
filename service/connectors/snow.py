885fa217613800520a1e2215e1e49873666c14e0
nlemay <nlemay@godaddy.com>
2018-03-16 10:13:48 -0700

    Add default timeouts for SNOW calls

--- service/connectors/snow.py
+++ service/connectors/snow.py
@@ -6,6 +6,8 @@ class SNOWHelper(object):
     get_headers = {'Accept': 'application/json'}
     post_headers = dict(list(get_headers.items()) + list({'Content-Type': 'application/json'}.items()))
 
+    default_timeout = 5  # Default timeout duration for all SNOW API calls
+
     def __init__(self, app_settings):
         self._url = app_settings.SNOW_URL
         self._user = app_settings.SNOW_USER
@@ -15,17 +17,21 @@ class SNOWHelper(object):
         with sessions.session() as session:
             return session.get(self._url + url_string,
                                auth=(self._user, self._pass),
-                               headers=self.post_headers)
+                               headers=self.post_headers,
+                               timeout=self.default_timeout)
 
     def patch_request(self, snow_table, json_data_string):
         with sessions.session() as session:
             return session.patch(self._url + snow_table,
                                  auth=(self._user, self._pass),
-                                 headers=self.post_headers, data=json_data_string)
+                                 headers=self.post_headers,
+                                 data=json_data_string,
+                                 timeout=self.default_timeout)
 
     def post_request(self, snow_table, json_data_string):
         with sessions.session() as session:
             return session.post(self._url + snow_table,
                                 auth=(self._user, self._pass),
                                 headers=self.post_headers,
-                                data=json_data_string)
+                                data=json_data_string,
+                                timeout=self.default_timeout)
